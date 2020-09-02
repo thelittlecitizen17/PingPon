24a62156d7dbe85468eba9def8f5d1047253d5e2
Yakov Pizenberg <thelittlecitizen17@gmail.com>
2020-09-02 21:14:57 +0300

    FirstAnswer

---
+++ PingPongClient/PingPongClient/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace PingPongClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the IP dect: ");
+            string ip = Console.ReadLine();
+            Console.WriteLine("Please enter the port number");
+            int port = int.Parse(Console.ReadLine());
+            StartClient(ip,port);
+        }
+        public static void StartClient(string ip , int port)
+        {
+            byte[] bytes = new byte[1024];
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry(ip);
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                Socket sender = new Socket(ipAddress.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    // Connect to Remote EndPoint  
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
+
+
+                    while (true)
+                    {
+                        Console.WriteLine("Please enter what you want to send: ");
+                        string message = Console.ReadLine();
+                        // Encode the data string into a byte array.    
+                        byte[] msg = Encoding.ASCII.GetBytes(message);
+
+                        // Send the data through the socket.    
+                        int bytesSent = sender.Send(msg);
+                        if(message=="exit")
+                        {
+                            break;
+                        }
+                        // Receive the response from the remote device.    
+                        int bytesRec = sender.Receive(bytes);
+                        Console.WriteLine($"Server sent = {Encoding.ASCII.GetString(bytes, 0, bytesRec)}");
+                    }
+                    // Release the socket.    
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
---
+++ PingPongServer/PingPongServer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace PingPongServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                StartServer(args);
+                Thread.Sleep(100000);
+            }
+        }
+        public static void StartServer(string[] args)
+        {
+
+            int port = int.Parse(args[0]);
+            IPHostEntry host = Dns.GetHostEntry("10.1.0.25");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(localEndPoint);
+                listener.Listen(20);
+
+
+                Console.WriteLine("Waiting for a connection...");
+
+                while (true)
+                {
+                    Socket handler = listener.Accept();
+                    Task task = Task.Run(() => getMessage(handler));
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }   
+
+
+        }
+        public static void getMessage(Socket handler)
+        {
+
+            string data = null;
+            byte[] bytes = null;
+
+            while (true)
+            {
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                if (data.IndexOf("exit") > -1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Text Received: {data}");
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    handler.Send(msg);
+                }
+            }
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+    }
+}
+          
